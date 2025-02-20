826bbf9176fb11daa4fc357145bac13e1517dceb
Adhvik Kannan <askannan@purdue.edu>
2025-02-19 20:27:00 -0500

    updated process_videos and added process_consistency_videos api endpoints to be more similar in handling and the page for Consistency upload

---
+++ frontend/ShotMatch/screens/ConsistencyScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationProp } from '@react-navigation/native';
+import Consistency from '../components/Consistency';
+
+interface Props {
+    navigation: NavigationProp<any>;
+}
+
+const ConsistencyScreen: React.FC<Props> = ({ navigation }) => {
+    return <Consistency navigation={navigation} />;
+};
+
+export default ConsistencyScreen;
