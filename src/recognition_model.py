efcac868791c05e5ce794cdd25377a900739da5d
rock <zhan4243@purdue.edu>
2025-04-02 22:07:08 -0400

    added waist and 0.1 sec measurement feature

--- src/recognition_model.py
+++ src/recognition_model.py
@@ -93,7 +93,7 @@ def eye_level_measurement(eye, mouth, ball, wrist, height):
         mouth_x, mouth_y = mouth
         if ball is not None:
             ball_x, ball_y = ball
-            ball[1] = h - ball_y
+            ball[1] = height - ball_y
             if ball_y < (2 * eye_y) - mouth_y and ball_y > (2 * mouth_y) - eye_y:
             # if ball_y < eye_y and ball_y > mouth_y:
                 frame_flag = True
@@ -106,22 +106,19 @@ def eye_level_measurement(eye, mouth, ball, wrist, height):
 
 
 # TODO: NOT DONE
-def waist_level_measurement(eye, mouth, ball, wrist):
+def waist_level_measurement(hip, wrist, shoulder, ball, height):
     frame_flag = False
-    #  球在眼睛的高度
-    if eye is not None and mouth is None:
-        eye_x, eye_y = eye
-        mouth_x, mouth_y = mouth
+    if hip is not None and shoulder is None:
+        hip_x, hip_y = hip
+        shoulder_x, shoulder_y = shoulder
         if ball is not None:
             ball_x, ball_y = ball
-            ball[1] = h - ball_y
-            if ball_y < (2 * eye_y) - mouth_y and ball_y > (2 * mouth_y) - eye_y:
-            # if ball_y < eye_y and ball_y > mouth_y:
+            ball[1] = height - ball_y
+            if ball_y < shoulder_y and ball_y > hip_y:
                 frame_flag = True
-        #  如果球不存在，那么看手腕的高度
         elif wrist is not None:
             wrist_x, wrist_y = wrist
-            if wrist_y < (2 * eye_y) - mouth_y and wrist_y > (2 * mouth_y) - eye_y:
+            if wrist_y < shoulder_y and wrist_y > hip_y:
                 frame_flag = True
     return frame_flag
                 
@@ -313,11 +310,103 @@ def analyze_video(video_path):
     #===================================== start of comment ===============================
     # cv2.destroyAllWindows()
     #===================================== end of comment =================================
-    if len(eye_level_data) != 0:
+    if len(eye_level_data) != 0 and len(waist_level_data) != 0:
+        min_waist = min(waist_level_data.keys())
         max_key = max(eye_level_data.keys())
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        offset_frame = int(max_key + fps * 0.1)
+        cap = cv2.VideoCapture(video_path)
+        cap.set(cv2.CAP_PROP_POS_FRAMES, offset_frame)
+        success, frame = cap.read()
+        rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = pose.process(rgb_image)
+        if results.pose_landmarks:
+            landmarks = results.pose_landmarks.landmark
+            h, w, _ = frame.shape
+            nose             = get_coordinate(landmarks, mp_pose.PoseLandmark.NOSE.value, w, h)
+            left_eye_inner   = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_EYE_INNER.value, w, h)
+            left_eye         = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_EYE.value, w, h)
+            left_eye_outer   = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_EYE_OUTER.value, w, h)
+            right_eye_inner  = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_EYE_INNER.value, w, h)
+            right_eye        = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_EYE.value, w, h)
+            right_eye_outer  = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_EYE_OUTER.value, w, h)
+            left_ear         = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_EAR.value, w, h)
+            right_ear        = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_EAR.value, w, h)
+            left_mouth       = get_coordinate(landmarks, mp_pose.PoseLandmark.MOUTH_LEFT.value, w, h)
+            right_mouth      = get_coordinate(landmarks, mp_pose.PoseLandmark.MOUTH_RIGHT.value, w, h)
+            left_shoulder    = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_SHOULDER.value, w, h)
+            right_shoulder   = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_SHOULDER.value, w, h)
+            left_elbow       = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_ELBOW.value, w, h)
+            right_elbow      = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_ELBOW.value, w, h)
+            left_wrist       = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_WRIST.value, w, h)
+            right_wrist      = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_WRIST.value, w, h)
+            left_pinky       = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_PINKY.value, w, h)
+            right_pinky      = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_PINKY.value, w, h)
+            left_index       = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_INDEX.value, w, h)
+            right_index      = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_INDEX.value, w, h)
+            left_thumb       = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_THUMB.value, w, h)
+            right_thumb      = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_THUMB.value, w, h)
+            left_hip         = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_HIP.value, w, h)
+            right_hip        = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_HIP.value, w, h)
+            left_knee        = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_KNEE.value, w, h)
+            right_knee       = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_KNEE.value, w, h)
+            left_ankle       = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_ANKLE.value, w, h)
+            right_ankle      = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_ANKLE.value, w, h)
+            left_heel        = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_HEEL.value, w, h)
+            right_heel       = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_HEEL.value, w, h)
+            left_foot_index  = get_coordinate(landmarks, mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value, w, h)
+            right_foot_index = get_coordinate(landmarks, mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value, w, h)
+            ball    = detect_ball(frame)
+            eye     = choose_valid_side(left_eye, right_eye)
+            mouth   = choose_valid_side(left_mouth, right_mouth)
+            wrist   = choose_valid_side(left_wrist, right_wrist)
+            pinky   = choose_valid_side(left_pinky, right_pinky)
+            hip     = choose_valid_side(left_hip, right_hip)
+            shoulder= choose_valid_side(left_shoulder, right_shoulder)
+            side    = detect_side(right_shoulder, right_elbow, right_wrist, left_shoulder, left_elbow, left_wrist)
+            frame_data = {
+                        "nose"            : nose            ,
+                        "left_eye_inner"  : left_eye_inner  ,
+                        "left_eye"        : left_eye        ,
+                        "left_eye_outer"  : left_eye_outer  ,
+                        "right_eye_inner" : right_eye_inner ,
+                        "right_eye"       : right_eye       ,
+                        "right_eye_outer" : right_eye_outer ,
+                        "left_ear"        : left_ear        ,
+                        "right_ear"       : right_ear       ,
+                        "left_mouth"      : left_mouth      ,
+                        "right_mouth"     : right_mouth     ,
+                        "left_shoulder"   : left_shoulder   ,
+                        "right_shoulder"  : right_shoulder  ,
+                        "left_elbow"      : left_elbow      ,
+                        "right_elbow"     : right_elbow     ,
+                        "left_wrist"      : left_wrist      ,
+                        "right_wrist"     : right_wrist     ,
+                        "left_pinky"      : left_pinky      ,
+                        "right_pinky"     : right_pinky     ,
+                        "left_index"      : left_index      ,
+                        "right_index"     : right_index     ,
+                        "left_thumb"      : left_thumb      ,
+                        "right_thumb"     : right_thumb     ,
+                        "left_hip"        : left_hip        ,
+                        "right_hip"       : right_hip       ,
+                        "left_knee"       : left_knee       ,
+                        "right_knee"      : right_knee      ,
+                        "left_ankle"      : left_ankle      ,
+                        "right_ankle"     : right_ankle     ,
+                        "left_heel"       : left_heel       ,
+                        "right_heel"      : right_heel      ,
+                        "left_foot_index" : left_foot_index ,
+                        "right_foot_index": right_foot_index,
+                        "ball"            : ball            ,
+                        "Side"            : side            ,
+                        "frame"           : frame_index     ,
+                        "postion"         : "AFTER_EYE"
+                    } 
         max_value = eye_level_data[max_key]
+        min_value = waist_level_data[min_waist]
         # return eye_level_data
-        return max_value
+        return min_value, max_value, frame_data
     else:
         return None
 
