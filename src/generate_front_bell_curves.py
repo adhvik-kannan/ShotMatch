4e8865d8075d97648f079ec50cb69d104a4b6fae
Adam <adamkhchen@gmail.com>
2025-04-21 05:18:22 -0400

    parse function

--- src/generate_front_bell_curves.py
+++ src/generate_front_bell_curves.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from scipy.stats import beta
+import ast
 
 NUM_CLIPS = 0
 
@@ -221,48 +222,82 @@ def plot_beta_distribution(params):
     # Show the plot
     plt.show()
 
-def get_steph_curry_front_data():
-    nba_18 = {'left_shoulder': [191, 466], 'right_shoulder': [125, 468], 'left_elbow': [215, 472], 'right_elbow': [136, 459], 'left_wrist': [205, 535], 'right_wrist': [152, 527], 'left_hip': [182, 326], 'right_hip': [140, 327], 'left_pinky': [198, 549], 'right_pinky': [159, 543], 'left_thumb': [197, 543], 'right_thumb': [156, 538], 'ball': None, 'Side': 'FRONT', 'frame': 63}
-    nba_19 = {'left_shoulder': [436, 656], 'right_shoulder': [362, 662], 'left_elbow': [474, 618], 'right_elbow': [352, 633], 'left_wrist': [460, 636], 'right_wrist': [380, 695], 'left_hip': [425, 521], 'right_hip': [382, 525], 'left_pinky': [454, 638], 'right_pinky': [389, 711], 'left_thumb': [446, 643], 'right_thumb': [387, 708], 'ball': None, 'Side': 'FRONT', 'frame': 45}
-    nba_20 = {'left_shoulder': [168, 533], 'right_shoulder': [101, 538], 'left_elbow': [196, 528], 'right_elbow': [108, 517], 'left_wrist': [184, 592], 'right_wrist': [127, 575], 'left_hip': [156, 391], 'right_hip': [113, 393], 'left_pinky': [179, 609], 'right_pinky': [132, 595], 'left_thumb': [177, 606], 'right_thumb': [130, 595], 'ball': None, 'Side': 'FRONT', 'frame': 15}
-    nba_22 = {'left_shoulder': [271, 508], 'right_shoulder': [207, 510], 'left_elbow': [313, 511], 'right_elbow': [218, 529], 'left_wrist': [277, 567], 'right_wrist': [227, 589], 'left_hip': [271, 363], 'right_hip': [228, 367], 'left_pinky': [270, 583], 'right_pinky': [232, 606], 'left_thumb': [266, 578], 'right_thumb': [231, 597], 'ball': None, 'Side': 'FRONT', 'frame': 37}
-    nba_23 = {'left_shoulder': [241, 610], 'right_shoulder': [173, 615], 'left_elbow': [271, 615], 'right_elbow': [176, 595], 'left_wrist': [254, 676], 'right_wrist': [196, 662], 'left_hip': [224, 469], 'right_hip': [182, 473], 'left_pinky': [250, 691], 'right_pinky': [200, 684], 'left_thumb': [246, 688], 'right_thumb': [194, 678], 'ball': None, 'Side': 'FRONT', 'frame': 19}
-    nba_24 = {'left_shoulder': [132, 747], 'right_shoulder': [63, 747], 'left_elbow': [161, 745], 'right_elbow': [73, 736], 'left_wrist': [145, 810], 'right_wrist': [88, 800], 'left_hip': [118, 603], 'right_hip': [74, 605], 'left_pinky': [136, 826], 'right_pinky': [96, 816], 'left_thumb': [134, 823], 'right_thumb': [90, 811], 'ball': None, 'Side': 'FRONT', 'frame': 11}
-    nba_26 = {'left_shoulder': [256, 666], 'right_shoulder': [186, 675], 'left_elbow': [294, 669], 'right_elbow': [201, 666], 'left_wrist': [277, 731], 'right_wrist': [214, 732], 'left_hip': [242, 513], 'right_hip': [195, 516], 'left_pinky': [269, 749], 'right_pinky': [217, 750], 'left_thumb': [266, 744], 'right_thumb': [213, 744], 'ball': None, 'Side': 'FRONT', 'frame': 21}
-    nba_27 = {'left_shoulder': [263, 723], 'right_shoulder': [192, 728], 'left_elbow': [290, 729], 'right_elbow': [198, 714], 'left_wrist': [277, 796], 'right_wrist': [216, 787], 'left_hip': [245, 570], 'right_hip': [199, 574], 'left_pinky': [267, 815], 'right_pinky': [217, 805], 'left_thumb': [267, 809], 'right_thumb': [217, 802], 'ball': None, 'Side': 'FRONT', 'frame': 10}
-
-    merged_dict = {}
-
-    for d in [nba_18, nba_19, nba_20, nba_22, nba_23, nba_24, nba_26, nba_27]:
-        for key, value in d.items():
-            if key not in merged_dict:
-                merged_dict[key] = [value]
-            else:
-                merged_dict[key].append(value)
-
+def prep_data(dict_list):
+    merged_dict = {
+        'left_shoulder': [], 'right_shoulder': [], 'left_elbow': [], 'right_elbow': [],
+        'left_wrist': [], 'right_wrist': [], 'left_hip': [], 'right_hip': [],
+        'left_pinky': [], 'right_pinky': [], 'left_thumb': [], 'right_thumb': [],
+        'ball': []
+    }
+    
+    for d in dict_list:
+        for key in merged_dict:
+            merged_dict[key].append(d.get(key, None))
+    
     return merged_dict
 
-def main():
-    sc_f_data = get_steph_curry_front_data()
-
-    sc_f_hew_ra, sc_f_hew_la, sc_f_sew_ra, sc_f_sew_la, sc_f_ewa_ra, sc_f_ewp_la = generate_front_parameters(sc_f_data)
-    sc_f_elbow = generate_elbow_parameters(sc_f_data)
-    print("\nSteph Curry Front-hew-ra params: ", sc_f_hew_ra)
-    print("\nSteph Curry Front-hew-la params: ", sc_f_hew_la)
-    print("\nSteph Curry Front-sew-ra params: ", sc_f_sew_ra)
-    print("\nSteph Curry Front-sew-la params: ", sc_f_sew_la)
-    print("\nSteph Curry Front-ewa-ra params: ", sc_f_ewa_ra)
-    print("\nSteph Curry Front-ewp-la params: ", sc_f_ewp_la)
-    print("\nSteph Curry Front-Elbow Params: ", sc_f_elbow)
-
-    # plot_beta_distribution(sc_f_hew_ra)
-    plot_beta_distribution(sc_f_hew_la)
-    # plot_beta_distribution(sc_f_sew_ra)
-    # plot_beta_distribution(sc_f_sew_la)
-    # plot_beta_distribution(sc_f_ewa_ra)
-    # plot_beta_distribution(sc_f_ewp_la)
-    # plot_beta_distribution(sc_f_elbow)
+def parse_data():
+    with open('data_steph.txt', 'r') as file:
+        lines = file.readlines()
     
+    data = {}
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue
+        player, view, dict_str = line.split(' ', 2)
+        player_view = f"{player}_{view}"
+        try:
+            data_dict = ast.literal_eval(dict_str)
+            data[player_view] = data_dict
+        except:
+            data[player_view] = None
+    
+    merged_data = {'waist': [], 'eye': [], 'max_hand': []}
+    for i in range(1, 30):
+        for view in ['waist', 'eye', 'max_hand']:
+            key = f"nba_{i}_{view}"
+            if key in data and data[key] is not None:
+                if 'Side' in data[key] and data[key]['Side'] == 'FRONT':
+                    merged_data[view].append(data[key])
+    
+    return merged_data
+
+def main():
+    # Parse data from data_steph.txt
+    data = parse_data()
+    print(f"data: {data}")
+    
+    for view in ['waist', 'eye', 'max_hand']:
+        if not data[view]:
+            print(f"No valid data for {view} view")
+            continue
+        
+        # prep data
+        sc_f_data = prep_data(data[view])
+        
+        # generate parameters
+        sc_f_hew_ra, sc_f_hew_la, sc_f_sew_ra, sc_f_sew_la, sc_f_ewa_ra, sc_f_ewp_la = generate_front_parameters(sc_f_data)
+        sc_f_elbow = generate_elbow_parameters(sc_f_data)
+        
+        # print parameters
+        print(f"\nSteph Curry {view.capitalize()} View Parameters:")
+        print(f"HEW Right Arm: {sc_f_hew_ra}")
+        print(f"HEW Left Arm: {sc_f_hew_la}")
+        print(f"SEW Right Arm: {sc_f_sew_ra}")
+        print(f"SEW Left Arm: {sc_f_sew_la}")
+        print(f"EWA Right Arm: {sc_f_ewa_ra}")
+        print(f"EWP Left Arm: {sc_f_ewp_la}")
+        print(f"Elbow Comparison: {sc_f_elbow}")
+        
+        # plot
+        plot_beta_distribution(sc_f_hew_ra, f"Beta Distribution for HEW Right Arm ({view.capitalize()})", f"hew_ra_{view}.png")
+        plot_beta_distribution(sc_f_hew_la, f"Beta Distribution for HEW Left Arm ({view.capitalize()})", f"hew_la_{view}.png")
+        plot_beta_distribution(sc_f_sew_ra, f"Beta Distribution for SEW Right Arm ({view.capitalize()})", f"sew_ra_{view}.png")
+        plot_beta_distribution(sc_f_sew_la, f"Beta Distribution for SEW Left Arm ({view.capitalize()})", f"sew_la_{view}.png")
+        plot_beta_distribution(sc_f_ewa_ra, f"Beta Distribution for EWA Right Arm ({view.capitalize()})", f"ewa_ra_{view}.png")
+        plot_beta_distribution(sc_f_ewp_la, f"Beta Distribution for EWP Left Arm ({view.capitalize()})", f"ewp_la_{view}.png")
+        plot_beta_distribution(sc_f_elbow, f"Beta Distribution for Elbow Comparison ({view.capitalize()})", f"elbow_{view}.png")
 
 if __name__ == "__main__":
     main()
