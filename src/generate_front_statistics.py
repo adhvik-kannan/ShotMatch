4324759d9f6aa05eda7e64c6206f84f3ae86599e
Adam <adamkhchen@gmail.com>
2025-04-29 16:02:42 -0400

    checking

--- src/generate_front_statistics.py
+++ src/generate_front_statistics.py
@@ -191,12 +191,12 @@ def compare_front(data, hew_ra_params, hew_la_params, sew_ra_params, sew_la_para
 
 
     
-    # plot_beta_with_point(angle_hew_ra, hew_ra_params, label="HEW_RA")
+    plot_beta_with_point(angle_hew_ra, hew_ra_params, label="HEW_RA")
     plot_beta_with_point(angle_hew_la, hew_la_params, label="HEW_LA")
-    # plot_beta_with_point(angle_sew_ra, sew_ra_params, label="SEW_RA")
-    # plot_beta_with_point(angle_sew_la, sew_la_params, label="SEW_LA")
-    # plot_beta_with_point(angle_ewa_ra, ewa_ra_params, label="EWA_RA")
-    # plot_beta_with_point(angle_ewp_la, ewp_la_params, label="EWP_LA")
+    plot_beta_with_point(angle_sew_ra, sew_ra_params, label="SEW_RA")
+    plot_beta_with_point(angle_sew_la, sew_la_params, label="SEW_LA")
+    plot_beta_with_point(angle_ewa_ra, ewa_ra_params, label="EWA_RA")
+    plot_beta_with_point(angle_ewp_la, ewp_la_params, label="EWP_LA")
     
     return {
         "Hip->Elbow->Wrist Score (Right Arm)": score_hew_ra,
@@ -209,7 +209,13 @@ def compare_front(data, hew_ra_params, hew_la_params, sew_ra_params, sew_la_para
     }
 
 def get_klay_data():
-    return {'left_shoulder': [654, 518], 'right_shoulder': [373, 476], 'left_elbow': [750, 323], 'right_elbow': [431, 291], 'left_wrist': [685, 584], 'right_wrist': [544, 570], 'left_hip': [651, 53], 'right_hip': [446, 51], 'left_pinky': [668, 656], 'right_pinky': [562, 633], 'left_thumb': [660, 641], 'right_thumb': [549, 624], 'ball': None, 'Side': 'FRONT', 'frame': 18}
+    Max_Front= {'nose': [983, 520], 'left_eye_inner': [984, 524], 'left_eye': [987, 524], 'left_eye_outer': [989, 524], 'right_eye_inner': [977, 524], 'right_eye': [975, 523], 'right_eye_outer': [972, 523], 'left_ear': [989, 515], 'right_ear': [965, 514], 'left_mouth': [988, 512], 'right_mouth': [979, 512], 'left_shoulder': [1009, 479], 'right_shoulder': [952, 472], 'left_elbow': [1028, 532], 'right_elbow': [961, 529], 'left_wrist': [1014, 594], 'right_wrist': [967, 593], 'left_pinky': [1006, 610], 'right_pinky': [969, 606], 'left_index': [1002, 611], 'right_index': [971, 610], 'left_thumb': [1003, 605], 'right_thumb': [972, 605], 'left_hip': [1010, 354], 'right_hip': [970, 354], 'left_knee': [1028, 259], 'right_knee': [968, 257], 'left_ankle': [1030, 160], 'right_ankle': [942, 157], 'left_heel': [1023, 149], 'right_heel': [932, 146], 'left_foot_index': [1051, 125], 'right_foot_index': [962, 114], 'frame': 39, 'time': 1.2792, 'postion': 'HAND'}
+    Eye_Front= {'nose': [1016, 444], 'left_eye_inner': [1019, 452], 'left_eye': [1021, 452], 'left_eye_outer': [1022, 452], 'right_eye_inner': [1012, 452], 'right_eye': [1008, 453], 'right_eye_outer': [1005, 453], 'left_ear': [1021, 449], 'right_ear': [996, 451], 'left_mouth': [1018, 437], 'right_mouth': [1009, 437], 'left_shoulder': [1036, 410], 'right_shoulder': [971, 404], 'left_elbow': [1064, 412], 'right_elbow': [985, 382], 'left_wrist': [1035, 465], 'right_wrist': [1003, 445], 'left_pinky': [1028, 479], 'right_pinky': [1002, 462], 'left_index': [1024, 479], 'right_index': [1000, 462], 'left_thumb': [1025, 473], 'right_thumb': [1000, 455], 'left_hip': [1026, 285], 'right_hip': [988, 284], 'left_knee': [1060, 202], 'right_knee': [992, 192], 'left_ankle': [1051, 122], 'right_ankle': [941, 117], 'left_heel': [1038, 107], 'right_heel': [926, 104], 'left_foot_index': [1076, 93], 'right_foot_index': [962, 82], 'frame': 30, 'time': 0.984, 'postion': 'EYE'}
+    Waist_Front= {'nose': [1009, 431], 'left_eye_inner': [1011, 439], 'left_eye': [1014, 438], 'left_eye_outer': [1016, 438], 'right_eye_inner': [1004, 439], 'right_eye': [1001, 439], 'right_eye_outer': [998, 438], 'left_ear': [1020, 433], 'right_ear': [992, 433], 'left_mouth': [1013, 423], 'right_mouth': [1004, 423], 'left_shoulder': [1041, 391], 'right_shoulder': [968, 383], 'left_elbow': [1056, 333], 'right_elbow': [956, 317], 'left_wrist': [1044, 330], 'right_wrist': [1003, 323], 'left_pinky': [1042, 323], 'right_pinky': [1016, 327], 'left_index': [1037, 330], 'right_index': [1017, 335], 'left_thumb': [1035, 332], 'right_thumb': [1012, 334], 'left_hip': [1022, 279], 'right_hip': [982, 279], 'left_knee': [1058, 203], 'right_knee': [989, 195], 'left_ankle': [1052, 120], 'right_ankle': [941, 115], 'left_heel': [1039, 104], 'right_heel': [932, 99], 'left_foot_index': [1075, 93], 'right_foot_index': [962, 81], 'frame': 26, 'time': 0.8528, 'postion': 'WAIST'}
+    Max_Side = {'nose': [584, 478], 'left_eye_inner': [579, 484], 'left_eye': [578, 484], 'left_eye_outer': [578, 484], 'right_eye_inner': [577, 483], 'right_eye': [575, 483], 'right_eye_outer': [573, 482], 'left_ear': [570, 479], 'right_ear': [564, 476], 'left_mouth': [584, 470], 'right_mouth': [582, 470], 'left_shoulder': [560, 437], 'right_shoulder': [566, 446], 'left_elbow': None, 'right_elbow': [621, 489], 'left_wrist': None, 'right_wrist': [647, 548], 'left_pinky': None, 'right_pinky': [661, 558], 'left_index': None, 'right_index': [658, 563], 'left_thumb': None, 'right_thumb': [653, 561], 'left_hip': [557, 309], 'right_hip': [573, 310], 'left_knee': None, 'right_knee': [595, 218], 'left_ankle': [581, 122], 'right_ankle': [606, 110], 'left_heel': [570, 106], 'right_heel': [596, 95], 'left_foot_index': [610, 99], 'right_foot_index': [636, 85], 'frame': 24, 'time': 0.7737142857142857, 'postion': 'HAND'}
+    Eye_Side = {'nose': [580, 389], 'left_eye_inner': [574, 397], 'left_eye': [573, 397], 'left_eye_outer': [572, 396], 'right_eye_inner': [574, 397], 'right_eye': [573, 397], 'right_eye_outer': [572, 397], 'left_ear': [562, 391], 'right_ear': [562, 390], 'left_mouth': [578, 380], 'right_mouth': [577, 380], 'left_shoulder': [540, 340], 'right_shoulder': [572, 345], 'left_elbow': None, 'right_elbow': [637, 329], 'left_wrist': None, 'right_wrist': [638, 385], 'left_pinky': None, 'right_pinky': [647, 401], 'left_index': None, 'right_index': [629, 401], 'left_thumb': None, 'right_thumb': [627, 395], 'left_hip': [528, 212], 'right_hip': [550, 215], 'left_knee': None, 'right_knee': [609, 137], 'left_ankle': [531, 61], 'right_ankle': [576, 37], 'left_heel': [516, 51], 'right_heel': [564, 23], 'left_foot_index': [553, 32], 'right_foot_index': [608, 23], 'frame': 14, 'time': 0.4513333333333333, 'postion': 'EYE'}
+    Waist_Side = {'nose': [587, 382], 'left_eye_inner': [581, 390], 'left_eye': [580, 391], 'left_eye_outer': [578, 391], 'right_eye_inner': [581, 390], 'right_eye': [580, 390], 'right_eye_outer': [579, 390], 'left_ear': [568, 386], 'right_ear': [570, 384], 'left_mouth': [584, 373], 'right_mouth': [583, 373], 'left_shoulder': [528, 339], 'right_shoulder': [574, 343], 'left_elbow': None, 'right_elbow': [558, 271], 'left_wrist': None, 'right_wrist': [610, 251], 'left_pinky': None, 'right_pinky': [622, 244], 'left_index': None, 'right_index': [627, 255], 'left_thumb': None, 'right_thumb': [622, 258], 'left_hip': [512, 219], 'right_hip': [539, 217], 'left_knee': None, 'right_knee': [583, 135], 'left_ankle': [518, 53], 'right_ankle': [568, 30], 'left_heel': [508, 46], 'right_heel': [558, 18], 'left_foot_index': [547, 37], 'right_foot_index': [608, 20], 'frame': 9, 'time': 0.29014285714285715, 'postion': 'WAIST'}
+    return Max_Front, Eye_Front, Waist_Front, Max_Side, Eye_Side, Waist_Side
 
 def get_params():
     """
@@ -226,9 +232,13 @@ def get_params():
     return hew_ra_params, hew_la_params, sew_ra_params, sew_la_params, ewa_ra_params, ewp_la_params, elbow_params
 def main():
     # Compute similarity scores for front view.
-    new_front_data = get_klay_data()
+    max_f, eye_f, waist_f, max_s, eye_s, waist_s = get_klay_data()
+    klay_data = [max_f, eye_f, waist_f]
     hew_ra_params, hew_la_params, sew_ra_params, sew_la_params, ewa_ra_params, ewp_la_params, elbow_params = get_params()
-    front_scores = compare_front(new_front_data, hew_ra_params, hew_la_params, sew_ra_params, sew_la_params, ewa_ra_params, ewp_la_params, elbow_params)
+
+    for d in klay_data:
+        front_scores = compare_front(d, hew_ra_params, hew_la_params, sew_ra_params, sew_la_params, ewa_ra_params, ewp_la_params, elbow_params)
+
     
     print("Front view similarity scores:")
     print(front_scores)
